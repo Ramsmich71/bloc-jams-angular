93a42fe594f1b35bd4f726e88ee9fcc067949abc
Ramsmich71 <ramsmich71@gmail.com>
2017-07-18 22:28:43 -0400

    checkpoint 5 completed

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+    { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+    { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+    { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+    { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+  ]
+};
+
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/10.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01' },
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '3:21' },
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15' }
+  ]
+};
+
+var albumAnimal = {
+  title: 'The Safari',
+  artist: 'Uncle Sam',
+  label: 'USA',
+  year: '1776',
+  albumArtUrl: 'assets/images/album_covers/06.png',
+  songs: [
+    { title: 'Lion', duration: '3:26' },
+    { title: 'Goat', duration: '5:45' },
+    { title: 'Rhino', duration: '3:30' },
+    { title: 'Elephant', duration: '4:21' },
+    { title: 'Tiger', duration: '2:54' }
+  ]
+};
