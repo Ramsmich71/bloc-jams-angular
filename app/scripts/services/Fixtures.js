d1e2ac269ca9f0a1da435be6a9a3101ecc6950c8
Ramsmich71 <ramsmich71@gmail.com>
2017-07-23 11:49:49 -0400

    assignment 6 completed

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,14 @@
       return albumPicasso;
     };
 
+    Fixtures.getCollection = function(numberOfAlbums) {
+      this.albums = [];
+      for (var i=0; i < 12; i++) {
+        this.albums.push(angular.copy(albumPicasso));
+      }
+      return this.albums;
+    };
+
     return Fixtures;
   }
 
